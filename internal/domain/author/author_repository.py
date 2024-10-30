3a067777243f8ca1a43e8d628b74fc2475f227ac
Rayendra Sabandar <86864902+RayendraSabandar@users.noreply.github.com>
2024-10-30 22:23:04 +0700

    Feat/author routes (#2)
    * feat(author): created create endpoint
    
    * fix(database): added TODO
    
    * feat(response): created error handler
    
    * fix(response): added custom_message variable in handleErr
    
    * feat(author): created find_by_id endpoint
    
    * feat(author): created update endpoint
    
    * feat(author): created delete endpoint
    
    * feat(author): added soft_deleted filter

--- internal/domain/author/author_repository.py
+++ internal/domain/author/author_repository.py
@@ -3,5 +3,21 @@ from internal.models.author_model import Author
 
 class AuthorRepository:
     @staticmethod
+    def create(author):
+        db.session.add(author)
+        db.session.commit()
+
+    @staticmethod
     def list():
-        return Author.query.all()
+        return Author.query.filter_by(
+            deleted_at = None
+        )
+    
+    @staticmethod
+    def find_by_id(id):
+        return db.get_or_404(Author, id)
+    
+    @staticmethod
+    def update(author):
+        db.session.commit()
+        return author
--- transport/http/controller/author_controller.py
+++ transport/http/controller/author_controller.py
@@ -1,11 +1,23 @@
 from internal.domain.author.author_service import AuthorService
 from transport.http.response.response import Response
+from flask import request
+from werkzeug.exceptions import NotFound
 
 class AuthorController:
     @staticmethod
+    def create():
+        try:
+            request_body = request.json
+            AuthorService.create(request_body)
+            return Response.handleResponse(None)
+        
+        except Exception as e:
+            return Response.handleErr(e, None)
+
+    @staticmethod
     def list():
         authors = AuthorService.list()
-        authors_list = [
+        res = [
             {
                 "id": author.id,
                 "name": author.name,
@@ -14,4 +26,52 @@ class AuthorController:
             } 
             for author in authors
         ]
-        return Response.handleResponse(authors_list)
+        return Response.handleResponse(res)
+    
+    @staticmethod
+    def find_by_id(author_id):
+        try:
+            res = AuthorService.find_by_id(author_id)
+            return {
+                    "id": res.id,
+                    "name": res.name,
+                    "bio": res.bio,
+                    "birth_date": res.birth_date
+                } 
+
+        except Exception as e:
+            if isinstance(e, NotFound):
+                return Response.handleErr(e, "author not found")
+            
+            return Response.handleErr(e, None)
+        
+    @staticmethod
+    def update(author_id):
+        try:
+            request_body = request.json
+            res = AuthorService.update(author_id, request_body)
+            return {
+                "id": res.id,
+                "name": res.name,
+                "bio": res.bio,
+                "birth_date": res.birth_date
+            } 
+
+        except Exception as e:
+            if isinstance(e, NotFound):
+                return Response.handleErr(e, "author not found")
+            
+            return Response.handleErr(e, None)
+
+        
+    @staticmethod
+    def delete(author_id):
+        try:
+            AuthorService.soft_delete(author_id)
+            return Response.handleResponse(None)
+        
+        except Exception as e:
+            if isinstance(e, NotFound):
+                return Response.handleErr(e, "author not found")
+            
+            return Response.handleErr(e, None)
