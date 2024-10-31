2d812abd799b33848abe25f69979c42c9a841205
Rayendra Sabandar <86864902+RayendraSabandar@users.noreply.github.com>
2024-10-31 14:12:59 +0700

    feat(author): list now uses ListPayload

--- internal/domain/author/author_repository.py
+++ internal/domain/author/author_repository.py
@@ -1,5 +1,6 @@
 from database.database import db
 from internal.models.author_model import Author
+from sqlalchemy import and_
 
 class AuthorRepository:
     @staticmethod
@@ -8,10 +9,22 @@ class AuthorRepository:
         db.session.commit()
 
     @staticmethod
-    def list():
-        return Author.query.filter_by(
+    def list(payload):
+        query = Author.query.filter_by(
             deleted_at = None
         )
+
+
+        if payload.name:
+            query = query.filter(Author.name.ilike(f"%{payload.name}%"))
+        if payload.start_birth_date:
+            print(f"payload start_birth_date {payload.start_birth_date}")
+            query = query.filter(Author.birth_date >= payload.start_birth_date)
+        if payload.end_birth_date:
+            print(f"payload end_birth_date {payload.end_birth_date}")
+            query = query.filter(Author.birth_date <= payload.end_birth_date)
+
+        return query.all()
     
     @staticmethod
     def find_by_id(id):
