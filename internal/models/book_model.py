7d7f6bf18092a36cf4aec78726da678b6ca63b64
Rayendra Sabandar <86864902+RayendraSabandar@users.noreply.github.com>
2024-10-31 13:28:50 +0700

    migration(author-book): added index

--- internal/models/book_model.py
+++ internal/models/book_model.py
@@ -5,10 +5,10 @@ class Book(db.Model):
     __tablename__ = 'books'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    description = db.Column(db.String(120), nullable=False)
-    publish_date = db.Column(db.DateTime, nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable=False)
+    title = db.Column(db.String(80), nullable=False, index=True)
+    description = db.Column(db.String(120), nullable=False, index=True)
+    publish_date = db.Column(db.DateTime, nullable=False, index=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable=False, index=True)
 
     created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
