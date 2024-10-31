52a693a8e92dbbbf027865dabd28cbd53cfcdb1b
Rayendra Sabandar <86864902+RayendraSabandar@users.noreply.github.com>
2024-10-31 14:23:56 +0700

    feat(book): created ListPayload

---
+++ internal/domain/book/book_payload.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from typing import Optional
+from datetime import datetime
+
+@dataclass
+class ListPayload:
+    title: Optional[str] = None
+    description: Optional[str] = None
+    start_publish_date: Optional[datetime] = None
+    end_publish_date: Optional[datetime] = None
